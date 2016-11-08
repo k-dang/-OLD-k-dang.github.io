de32650e911827bee1c7cb30404781673053ba63
k-dang <dangkevin12@gmail.com>
2016-11-08 15:35:03 -0500

    added jquery animations to text

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 $("#about").click(function(){
-    console.log("abouts");
+    XMLReqTextFile("js/about.txt");
 });
 
 $("#contacts").click(function(){
-    console.log("conts");
+    $("#maintext p").remove();
+    $("#maintext").append("<p>email : dangkevin12@gmail.com</p>");
+    $("#maintext").append("<p>github : https://github.com/k-dang</p>");
+    $("#maintext").append("<p>linkedin : https://ca.linkedin.com/in/kdang1")
+    $("#maintext p").show('slow');
 });
 
 $(window).resize(function(){
@@ -11,3 +15,17 @@ $(window).resize(function(){
         console.log($(window).width());
     }
 });
+
+function XMLReqTextFile(file){
+    var fileReq = new XMLHttpRequest();
+    fileReq.open("GET",file,false);
+    fileReq.onreadystatechange = function (){
+        if(fileReq.readyState === 4){
+            if(fileReq.status === 200 || rawFile.status == 0){
+                var paragraphText = fileReq.responseText;
+                alert(paragraphText);
+            }
+        }
+    }
+    fileReq.send(null);
+}
