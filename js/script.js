8a8fad19456fbc87adb05e25e813718946255c8d
Kevin Dang <dangkevin12@gmail.com>
2017-01-23 22:19:10 -0500

    adding copy functionality with tooltips

--- js/script.js
+++ js/script.js
@@ -10,6 +10,8 @@ $("#contacts").click(function(){
     $("#maintext").append(getFaLink("github","https://github.com/k-dang"));
     $("#maintext").append(getFaLink("linkedin-square","https://ca.linkedin.com/in/kdang1"));
     $("#maintext div").show('slow');
+    $('#faGitText').tooltip();
+    $('#faLinkText').tooltip();
 });
 
 $(window).resize(function(){
@@ -33,9 +35,57 @@ function XMLReqTextFile(file){
 }
 
 function getFontAwesome(name){
-    return "<i class=\"fa fa-"+name+"\" aria-hidden=\"true\"></i>";
+    return '<i class="fa fa-'+name+'" aria-hidden="true"></i>';
 }
 
 function getFaLink(Icon, Text){
-    return "<div id=\"faicon\">"+getFontAwesome(Icon)+"<div id=\"fatext\">"+Text+"</div></div>";
+    var id;
+    switch(Icon){
+        case 'envelope':
+            id = 'faEnvText';
+            break;
+        case 'github':
+            id = 'faGitText'
+            break;
+        case 'linkedin-square':
+            id = 'faLinkText'
+            break;
+        default:
+    }
+    return "<div id=\"faicon\">"+getFontAwesome(Icon)+"<div class=\"fatext\" id="+id+" title=\"Copied\">"+Text+"</div></div>";
 }
+
+var clipboard = new Clipboard('#faEnvText', {
+    text: function() {
+        var Text = $('#faEnvText').text();
+        return Text;
+    }
+});
+
+
+var clipboard2 = new Clipboard('#faGitText', {
+    text: function() {
+        var Text = $('#faGitText').text();
+        return Text;
+    }
+});
+
+var clipboard3 = new Clipboard('#faLinkText', {
+    text: function() {
+        var Text = $('#faLinkText').text();
+        return Text;
+    }
+});
+
+// function copyMe(Text,id) {
+clipboard.on('success',function(e){
+    var clicked = e.trigger.id;
+    console.log(clicked);
+    var anchor = $('#'+clicked);
+    anchor.attr('data-original-title','Copied');
+    anchor.tooltip('show');
+});
+// }
+
+
+
