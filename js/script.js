424d8734f7ba324f881fbc237fdb260195e30316
Kevin Dang <dangkevin12@gmail.com>
2017-09-14 16:43:22 -0400

    Revamp

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 $("#about").click(function(){
     $("#maintext a").remove();
     $("#maintext #desc").remove();
-    XMLReqTextFile("js/about.txt");
     $("#maintext #desc").show('slow');
 });
 
@@ -19,39 +18,7 @@ $(window).resize(function(){
     }
 });
 
-function XMLReqTextFile(file){
-    var fileReq = new XMLHttpRequest();
-    fileReq.open("GET",file,false);
-    fileReq.onreadystatechange = function (){
-        if(fileReq.readyState === 4){
-            if(fileReq.status === 200 || rawFile.status == 0){
-                var paragraphText = fileReq.responseText;
-                $("#maintext").append("<div id=\"desc\">"+paragraphText+"</div>");
-            }
-        }
-    }
-    fileReq.send(null);
-}
-
-function getFontAwesome(name){
-    return '<i class="fa fa-3x fa-'+name+'" aria-hidden="true"></i>';
-}
-
-function getFaLink(Icon, Text){
-    var id;
-    switch(Icon){
-        case 'envelope':
-            id = 'faEnvText';
-            return "<a id=\"faicon\" href=\"mailto:dangkevin12@gmail.com\">"+getFontAwesome(Icon)+"</div>";
-        case 'github':
-            id = 'faGitText';
-            return "<a id=\"faicon\" href=\""+Text+"\">"+getFontAwesome(Icon)+"</div>";
-        case 'linkedin-square':
-            id = 'faLinkText';
-            return "<a id=\"faicon\" href=\""+Text+"\">"+getFontAwesome(Icon)+"</div>";
-        default:
-    }
-}
+{/* <a id=\"faicon\" href=\"mailto:dangkevin12@gmail.com\">"+getFontAwesome(Icon)+"</div> */}
 
 
 
