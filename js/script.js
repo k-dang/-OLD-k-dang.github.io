1d450483e5ef3e8276a65d7c1108e070844c58df
Kevin Dang <dangkevin12@gmail.com>
2017-09-15 02:34:42 -0400

    main scripts

--- js/script.js
+++ js/script.js
@@ -1,24 +1,49 @@
-$("#about").click(function(){
-    $("#maintext a").remove();
-    $("#maintext #desc").remove();
-    $("#maintext #desc").show('slow');
-});
-
-$("#contacts").click(function(){
-    $("#maintext a").remove();
-    $("#maintext").append(getFaLink("envelope","dangkevin12@gmail.com"));
-    $("#maintext").append(getFaLink("github","https://github.com/k-dang"));
-    $("#maintext").append(getFaLink("linkedin-square","https://ca.linkedin.com/in/kdang1"));
-    $("#maintext").show('slow');
-});
-
 $(window).resize(function(){
     if($(window).width() <= 600){
         console.log($(window).width());
     }
 });
 
-{/* <a id=\"faicon\" href=\"mailto:dangkevin12@gmail.com\">"+getFontAwesome(Icon)+"</div> */}
+$("#icon-group .fa").click(function(){
+    var className = $(this).attr("class");
+    var currentActive = className.split(" ");
+    var removeList = getNameOfOthers(currentActive[1]);
+    for(i=0;i<removeList.length;i++){
+        $(`.${removeList[i]}`).removeClass("active");
+    }
+    $(this).addClass("active")
+})
 
+$(".fa-home").click(function(){
+    $("#contact").fadeOut(300,function(){
+        $("#about").fadeIn(800);
+    })
+});
 
+$(".fa-envelope").click(function(){
+    $("#about").fadeOut(300,function(){
+        $("#contact").fadeIn(800);
+    });
+    
+});
+
+$(".fa-file-text").click(function(){
+    console.log("resume");
+});
+
+function getNameOfOthers(iconName){
+    var result;
+    switch(iconName){
+        case "fa-envelope":
+            result = ["fa-home","fa-file-text"]
+            break;
+        case "fa-home":
+            result = ["fa-envelope","fa-file-text"];
+            break;
+        case "fa-file-text":
+            result = ["fa-home","fa-envelope"];
+            break;
+    }
+    return result;
+}
 
