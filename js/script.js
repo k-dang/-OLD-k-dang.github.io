b82c8d9343ed91293f1b9df04cbcfa86f718b4b8
k-dang <dangkevin12@gmail.com>
2018-06-07 01:24:08 -0400

    clean up layout

--- js/script.js
+++ js/script.js
@@ -7,33 +7,32 @@ $(function() {
             $(this).addClass("active");
         });
     });
-    
+
     $(".about").click(function(){
         $("#contact").fadeOut(300,function(){
             $("#about").fadeIn(800);
         })
     });
-    
+
     $(".contact").click(function(){
         $("#about").fadeOut(300,function(){
             $("#contact").fadeIn(800);
         });
-        
     });
-    
-    function loadText(){
-        var xhr = new XMLHttpRequest();
-        xhr.open("GET","https://api.github.com/users/k-dang",true);
-        xhr.onload = function(){
-            if(this.status == 200){
-                var req = JSON.parse(this.responseText);
-                var gitAvatar = req["avatar_url"];
-                $("#profile").attr("src",gitAvatar);
-                // console.log(this.responseText);
-            }
-        }
-        xhr.send();
-    }
-    
+
     loadText();
 });
+
+function loadText(){
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET","https://api.github.com/users/k-dang",true);
+    xhr.onload = function(){
+        if(this.status == 200){
+            var req = JSON.parse(this.responseText);
+            var gitAvatar = req["avatar_url"];
+            $("#profile").attr("src", gitAvatar);
+            // console.log(this.responseText);
+        }
+    }
+    xhr.send();
+}
